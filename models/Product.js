e38a3062a4b2a9f4ee274e74a12588c9e0e9880b
Iury Handric <handric11@hotmail.com>
2025-06-16 23:01:33 -0300

    Resgatando valores do banco de dados

--- models/Product.js
+++ models/Product.js
@@ -5,24 +5,35 @@ const conn = require('../db/conn')
 class Product {
 
 // Criando o construtor com as propriedades   
-    constructor(name, price, description) {
+    constructor(name, image, price, description) {
 
 // Associando as propriedades do objeto
         this.name = name
+        this.image = image
         this.price = price
         this.description = description
     }
-// Método save para salvar (função criada)
+// Método save para salvar (função criada) CREATE
     save() {
 // Método para enviar ao banco de dados, passando os parâmetros da classe
         const product = conn.db().collection('products').insertOne({
             name: this.name,
+            image: this.image,
             price: this.price,
             description: this.description
         })
 
         return product
     }
+
+// Método para resgatar valores do banco
+    static getProducts(){
+
+        const products = conn.db().collection('products').find().toArray()
+
+        return products
+    }
+
 }
 
 module.exports = Product
